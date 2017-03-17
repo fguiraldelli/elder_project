04be3926e32225c3de7fa423c614ac58aec0750e
Francisco Guiraldelli <faccbg@cesar.org.br>
2017-03-17 14:12:42 -0300

    Some improvements in Enterprise class and rank.py

--- enterprise.py
+++ enterprise.py
@@ -1,18 +1,20 @@
 class Enterprise:
 	'Common base class for all enterprises'
+
+	list_of_questions = []
+	enterprise_name = ""
 	countEnterprise = 0
 	valid_num = 0
-	def __init__(self, enterprise_name, fav_num, neutral_num, unfav_num,
-	 invalid_num):
-		self.enterprise_name = enterprise_name
-		self.fav_num = fav_num
-		self.neutral_num = neutral_num
-		self.unfav_num = unfav_num
-		self.invalid_num = invalid_num
+	fav_num = 0
+	neutral_num = 0
+	unfav_num = 0
+	invalid_num = 0
+
+	def __init__(self):
 		Enterprise.countEnterprise += 1
 
 	def displayVariables(self):
-		print (	"Enterprise name: 	{}\n" 
+		print (	"{}\n" 
 				"Favoral checks: 	{}\n" 
 				"Unfavoral checks: 	{}\n" 
 				"Neutral checks: 	{}\n" 
@@ -26,8 +28,3 @@ class Enterprise:
 
 	def displayInvalidNumbers(self):
 		print ("Total of invalid numbers: {}\n".format(self.invalid_num))
-
-emp1 = Enterprise('XPTO', 500, 150, 100, 250)
-emp1.displayVariables()
-emp1.displayValidNumbers()
-emp1.displayInvalidNumbers()
--- rank.py
+++ rank.py
@@ -1,19 +1,22 @@
 import sys
 import fileinput
+from enterprise import Enterprise
 
 #read the input arguments and remove argument in index 0 of the list
 list = sys.argv
-#list.remove(list[0])
-print('\n')
-print('\n')
 list.remove(list[0])
-print (list)
-print('\n')
-name = ""
+
+#Create a list of enterprises classes
+list_of_enterprises = []
+number_of_enterprises = len(list)
+for i in range(number_of_enterprises):
+    list_of_enterprises.append(Enterprise())
+#Read each file and store the data in a class for each enterprise
+count_enterprise = 0
 with fileinput.input(files=((list))) as f:
-	for line in f:
-		if fileinput.isfirstline():
-			name = line
-		else:
-			
-	print (name)
+    for line in f:
+        if fileinput.isfirstline():
+            list_of_enterprises[count_enterprise].enterprise_name = line
+            count_enterprise += 1 
+for i in range(number_of_enterprises):
+    print (list_of_enterprises[i].enterprise_name)
