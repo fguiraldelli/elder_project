06bcf6101f4a99b0b1b1977a5b966ac783735b0c
fguira <francisco.guiraldelli@gmail.com>
2017-03-17 01:24:53 -0300

    Read file line by line

--- rank.py
+++ rank.py
@@ -2,13 +2,18 @@ import sys
 import fileinput
 
 #read the input arguments and remove argument in index 0 of the list
-list = str(sys.argv)
+list = sys.argv
 #list.remove(list[0])
 print('\n')
 print('\n')
+list.remove(list[0])
 print (list)
 print('\n')
-print (list[0])
-# with fileinput.input(files=('spam.txt', 'eggs.txt')) as f:
-#     for line in f:
-#         process(line)
+name = ""
+with fileinput.input(files=((list))) as f:
+	for line in f:
+		if fileinput.isfirstline():
+			name = line
+		else:
+			
+	print (name)
