5a02402087151479c1fcd3f654ee18f868721196
fguira <francisco.guiraldelli@gmail.com>
2017-03-19 20:40:14 -0300

    Great improvement in logic, functions and Enterprise class structure

--- enterprise.py
+++ enterprise.py
@@ -1,30 +1,42 @@
+from collections import OrderedDict
+
 class Enterprise:
-	"""Common base class for all enterprises"""
+    """Common base class for all enterprises"""
+    countEnterprise = 0
+    def __init__(self, name, valid_num, invalid_num, survey):
+        self.enterprise_name = name
+        self.valid_num = valid_num
+        self.invalid_num = invalid_num
+        self.survey = survey
+        Enterprise.countEnterprise += 1
 
-	list_of_questions = []
-	enterprise_name = ""
-	countEnterprise = 0
-	valid_num = 0
-	fav_num = 0
-	neutral_num = 0
-	unfav_num = 0
-	invalid_num = 0
+    def sort_survey():
+        return OrderedDict(sorted(survey.items()))
 
-	def __init__(self):
-		Enterprise.countEnterprise += 1
+    def displayVariables(self):
+        print ( "{}\n" 
+                "Favoral checks:    {}\n" 
+                "Unfavoral checks:  {}\n" 
+                "Neutral checks:    {}\n" 
+                "Invalid checks:    {}\n"
+                .format(self.enterprise_name, self.fav_num, self.unfav_num,
+                        self.neutral_num, self.invalid_num))
 
-	def displayVariables(self):
-		print (	"{}\n" 
-				"Favoral checks: 	{}\n" 
-				"Unfavoral checks: 	{}\n" 
-				"Neutral checks: 	{}\n" 
-				"Invalid checks: 	{}\n"
-				.format(self.enterprise_name, self.fav_num, self.unfav_num,
-						self.neutral_num, self.invalid_num))
+    def displayValidNumbers(self):
+        print ("The valid pool's number is: {}\n".format(self.fav_num
+            +self.neutral_num+self.unfav_num))
 
-	def displayValidNumbers(self):
-		print ("The valid pool's number is: {}\n".format(self.fav_num
-			+self.neutral_num+self.unfav_num))
+    def displayInvalidNumbers(self):
+        print ("Total of invalid numbers: {}\n".format(self.invalid_num))
 
-	def displayInvalidNumbers(self):
-		print ("Total of invalid numbers: {}\n".format(self.invalid_num))
+    def displayStatistics(self):
+        print(
+            "Enterprise name:   {}\n"
+            "Valid answers:     {}\n"
+            "Invalid answers:   {}\n"
+            "Survey:            {}\n"
+            .format(self.enterprise_name,
+                self.valid_num,
+                self.invalid_num,
+                self.survey)
+            )
--- rank.py
+++ rank.py
@@ -1,17 +1,15 @@
 import sys
 import fileinput
+import time
 from enterprise import Enterprise
 from question import Question
 from collections import OrderedDict
 
-def create_file_list_and_enterprise(list):
+def create_file_list(list):
     """ Function that reads the inputed arguments and returns a two lists: 
     files and enterprises"""
-    print (list)
     list.remove(list[0])
     list_of_enterprises = []
-    for i in range(len(list)):
-        list_of_enterprises.append(Enterprise())
     return list_of_enterprises, list
 
 def count_answer_type(dict_type_question, answer):
@@ -30,39 +28,55 @@ def init_question(answer):
     dict_type_question ={'fav':0, 'neutral':0, 'unfav':0}
     return count_answer_type(dict_type_question, answer)
 
+def sort_survey(survey):
+    return OrderedDict(sorted(survey.items()))
+
+def filled_class(enterprise_name, survey, invalid_num, count_line):
+    valid_num = count_line - invalid_num
+    new_class = Enterprise(enterprise_name, valid_num, invalid_num, survey)
+    return new_class
+
 def read_files_from_input(list_of_files, list_of_enterprises):
     """Function that reads each file and store the data in a class 
     for each enterprise"""
     count_enterprise = 0
-    count_invalid = 0
-    dict_question = {}
-    
-
     with fileinput.input(files=((list_of_files))) as f:
+        survey = {}
+        enterprise_name = ''
+        count_line = 0
         for line in f:
+            line = line.strip()
             if fileinput.isfirstline():
-                print(fileinput.filename())
-                list_of_enterprises[count_enterprise].enterprise_name = line
+                print('File line number First Line: '+str(fileinput.filelineno()))
+                print('Line number First Line: '+str(fileinput.lineno()))
+                if fileinput.filelineno() < fileinput.lineno():
+                    list_of_enterprises.append(filled_class(enterprise_name,
+                        survey, count_invalid, count_line))
+                #time.sleep(5)
+                survey = {}
+                count_line = 0
+                count_invalid = 0
+                enterprise_name = line
                 count_enterprise += 1 
             else:
                 question_number, answer = map(int,line.split(' '))
-                print(question_number,answer)
+                # print(question_number,answer)
                 if answer < 0 or answer > 4:
                     count_invalid += 1
-                elif question_number not in dict_question.keys():
-                    dict_question[question_number] = init_question(answer)
+                elif question_number not in survey.keys():
+                    survey[question_number] = init_question(answer)
                 else:
-                    dict_question[question_number] = count_answer_type\
-                    (dict_question[question_number], answer)
-        dict_question = OrderedDict(sorted(dict_question.items()))
-        print(dict_question)
-        print ('INCONSISTENT: '+ str(count_invalid))
-        count_invalid = 0
-    for i in range(len(list_of_enterprises)):
-        print (list_of_enterprises[i].enterprise_name)
-
-
+                    survey[question_number] = count_answer_type\
+                    (survey[question_number], answer)
+            count_line += 1
+            print('Line number: '+str(fileinput.lineno()))
+            print('File line number: '+str(fileinput.filelineno()))
+            # list_of_enterprises[count_enterprise-1].survey = survey
+        list_of_enterprises.append(filled_class(enterprise_name,
+            survey, count_invalid, count_line))
+        for i in range(count_enterprise):
+            list_of_enterprises[i].displayStatistics()
 
 #read the input arguments and remove argument in index 0 of the list
-list_of_enterprises, list_of_files = create_file_list_and_enterprise(sys.argv)
+list_of_enterprises, list_of_files = create_file_list(sys.argv)
 read_files_from_input(list_of_files, list_of_enterprises)
